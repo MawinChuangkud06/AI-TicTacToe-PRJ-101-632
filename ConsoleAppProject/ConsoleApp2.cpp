a899e05881eba97b3539b1d9b93d5e1ce01d0515
Mawin Chaungkud <weena.chuangkud@gmail.com>
2025-03-23 12:28:59 +0700

    Update ConsoleApp2.cpp

--- ConsoleAppProject/ConsoleApp2.cpp
+++ ConsoleAppProject/ConsoleApp2.cpp
@@ -1217,8 +1217,8 @@ private:
 
 int main()
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+    std::ios::sync_with_stdio(0);
+    std::cin.tie(0);
     TicTacToe T1;
     T1.ChooseGameModeAndPlay();
 
